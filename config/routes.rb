6c963c4bde611abe87efc13916878749be309da3
yohei222 <9oclockyohey@gmail.com>
2020-04-24 08:27:31 +0900

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
   get  '/about', to: 'static_pages#about'
   get  '/signup', to: 'users#new'
   resources :users
   post '/signup', to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
