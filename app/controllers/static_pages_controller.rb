d633c99ffdd4c61088d190419c191fa1e2de4eec
yohei222 <9oclockyohey@gmail.com>
2020-04-26 18:53:48 +0900

    ransackでuserをnameで検索できるように実装

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost  = current_user.microposts.build
-      @feed_items = Micropost.all.paginate(page: params[:page])
+      @feed_items = Micropost.all.paginate(page: params[:page], per_page: 5)
     end
   end
 
