7f466a53971f4ce4f63f3daffa4c9a264ba0eb33
yohei222 <9oclockyohey@gmail.com>
2020-04-26 18:59:38 +0900

    paginateを追記

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost  = current_user.microposts.build
-      @feed_items = Micropost.all.paginate(page: params[:page], per_page: 5)
+      @feed_items = Micropost.all.paginate(page: params[:page], per_page: 7)
     end
   end
 
