6ad808710bf6095f680b9da9cc0367a8bf55598c
yohei222 <9oclockyohey@gmail.com>
2020-04-30 01:55:02 +0900

    usersを降順に表示されるように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   enum sex: { man: 0, woman: 1, unanswered: 2 }
   enum status: { nonreleased: 0, released: 1 }
+  default_scope -> { order(created_at: :desc) }
   attr_accessor :remember_token
   mount_uploader :image, ImageUploader
   before_save { self.email = email.downcase }
